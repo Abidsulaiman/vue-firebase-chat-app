f835d4a641c07805d3db84b3b60df2095a8b6b35
abidsulaiman <abidqubit@gmail.com>
2021-03-13 16:14:58 +0530

    updated

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@
 
 module.exports = {
   publicPath:
-    process.env.NODE_ENV === "production" ? "/vue-firebase-chat-app/" : "/",
+    process.env.NODE_ENV === "production" ? "/vue-firebase-chat-app/dist" : "/",
 };
